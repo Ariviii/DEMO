78c2e708ffb231f6cf2570fd87c2e37c6feb6703
Ariviii <162411005+Ariviii@users.noreply.github.com>
2024-09-18 15:25:59 +0530

    Add files via upload

---
+++ FastAPI.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI # type: ignore
+from pydantic import BaseModel
+
+import uvicorn
+app = FastAPI()
+
+class Filename(BaseModel):
+    Data:str
+    Status: list
+
+
+
+
+@app.get("/get_api")
+def Sense():
+    return "get Application Interface"
+
+
+@app.post("/sensor_data")
+def receive_sensor_data(data:Filename):
+    print("\n[<---]Reciving Data: ",type(data),"\n")
+    print("\n[Live Data]: ",data)
+    return "200"
+
+
+
+
+if __name__ == "__main__":
+   uvicorn.run('FastAPI:app', host='localhost', port=9004, workers=2)    
+    #  uvicorn.run('main:app', host='0.0.0.0', port=9004, workers=2)
